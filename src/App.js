5e346134ea177acc9453bcc9e9bd1ac52ec63692
Lucas Meza <lucasezequiel9720@hotmail.com>
2022-08-10 17:13:11 -0300

    Cambio fetch itemListContainer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import './CSS/App.css';
 import { Routes, Route } from "react-router-dom";
 import NavBar from './Components/NavBar/NavBar';
-<<<<<<< main
 import ItemListConteiner from './Components/ContenedorProductos/ListaProductos/ItemListConteiner';
 import ItemDetailsConteiner from './Components/ContenedorProductos/DetalleProducto/ItemDetailsConteiner';
-=======
-import ItemListConteiner from './Components/PerrosAdopcion/ListaPerros/ItemListConteiner';
-import ItemDetailsConteiner from './Components/PerrosAdopcion/DetallePerros/ItemDetailsConteiner';
 import CartContext from './Context/CartContext';
 
->>>>>>> local
 
 function App() {
   return (
     <div className="App">
-<<<<<<< main
       <NavBar />
-      <Routes>
-        <Route exact path='/' element={<ItemListConteiner />} />
-        <Route exact path='/item/:id' element={<ItemDetailsConteiner />} />
-        <Route path='/categoria/:categoria' element={<ItemListConteiner />} />
-      </Routes>
-=======
       <CartContext.Provider value={[]}>
         <NavBar />
         <Routes>
@@ -30,8 +18,7 @@ function App() {
           <Route path='/categoria/:categoria' element={<ItemListConteiner />} />
         </Routes>
       </CartContext.Provider>
->>>>>>> local
-    </div>
+    </div >
   );
 }
 
