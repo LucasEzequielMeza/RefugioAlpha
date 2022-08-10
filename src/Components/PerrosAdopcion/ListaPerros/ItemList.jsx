f219a414a22735505af075b23227adb4c4211d6a
Lucas Meza <lucasezequiel9720@hotmail.com>
2022-08-10 17:28:14 -0300

    cambios fetch ItemListContainer

--- src/Components/PerrosAdopcion/ListaPerros/ItemList.jsx
+++ src/Components/PerrosAdopcion/ListaPerros/ItemList.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import Item from './Item'
 import './../../../CSS/ItemList.css'
 import ItemProductos from '../../ContenedorProductos/ListaProductos/ItemProductos'
+<<<<<<< main
+=======
+
+>>>>>>> local
 const ItemList = ({ item }) => {
 
     return (
         <div className='conteiner contenedorItemlist'>
+<<<<<<< main
             <div className='perros'>
+=======
+            <div className='perros productos'>
+>>>>>>> local
                 {item === '../Data/perros.json' ? item.map((item) => <Item key={item.id} perro={item} />) : item.map((item) => <ItemProductos key={item.id} producto={item} />)}
             </div>
         </div>
